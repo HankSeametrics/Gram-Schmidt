828e5210909c586c8a436cd571c23950a00d2493
HankSeametrics <hbeck@seametrics.com>
2019-07-01 16:18:56 -0700

    merged with master

--- GS.py
+++ GS.py
@@ -1,5 +1,14 @@
 #This is a program written to explore the functionality of git by building a program from scratch
 import numpy as np
+import time
+
+#Useful operators
+def projUV(U,V):
+    vu=np.dot(V,U)
+    uu=np.dot(U,U)
+    proj = (float(vu)/uu)*U
+    time.sleep(30)
+    return proj
 
 
 
