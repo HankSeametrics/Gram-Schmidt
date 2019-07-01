28aac617112b3741d4d9989430430fdb88f45c2f
HankSeametrics <hbeck@seametrics.com>
2019-07-01 11:19:58 -0700

    added projection operator

--- GS.py
+++ GS.py
@@ -1,2 +1,8 @@
 #This is a program written to explore the functionality of git by building a program from scratch
 import numpy as np
+
+def projUV(U,V):
+    vu=np.inner(V,U)
+    uu=np.inner(U,U)
+    proj = (vu/uu)*U
+    return proj
