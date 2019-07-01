464dcbe5ed6996790e8f71b3643d86efa72500e6
HankSeametrics <hbeck@seametrics.com>
2019-07-01 14:54:11 -0700

    Adding the GS method

--- GS.py
+++ GS.py
@@ -1,2 +1,14 @@
 #This is a program written to explore the functionality of git by building a program from scratch
 import numpy as np
+
+
+
+
+
+
+
+
+
+def GS_2vectors(*argv):
+    for arg in argv:
+        U=arg-np.sum()
