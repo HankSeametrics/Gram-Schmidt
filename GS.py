844bf49f74881cb8b07610bd0b71cf6faa8ede7b
HankSeametrics <hbeck@seametrics.com>
2019-07-01 14:22:30 -0700

    fixed division to return float

--- GS.py
+++ GS.py
@@ -1,8 +1,11 @@
 #This is a program written to explore the functionality of git by building a program from scratch
 import numpy as np
+import time
 
+#Useful operators
 def projUV(U,V):
-    vu=np.inner(V,U)
-    uu=np.inner(U,U)
-    proj = (vu/uu)*U
+    vu=np.dot(V,U)
+    uu=np.dot(U,U)
+    proj = (float(vu)/uu)*U
+    time.sleep(30)
     return proj
